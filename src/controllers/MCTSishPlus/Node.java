a73787a2fb8a1f4705069d8dbccaaeb111558219
torybash <torybash@gmail.com>
2015-01-30 15:07:51 +0100

    huh? why does this need commit?

--- src/controllers/MCTSishPlus/Node.java
+++ src/controllers/MCTSishPlus/Node.java
@@ -1,1 +1,20 @@
+package controllers.MCTSishPlus;
+
+import java.util.LinkedList;
+
+import tools.Vector2d;
+import core.game.StateObservation;
+
+public class Node {
+	public StateObservation state;
+	public LinkedList<Integer> list = new LinkedList<Integer>();
+	public Vector2d lastAvatarPos;
+	
+	public Node(StateObservation state, LinkedList<Integer> list, Vector2d lastAvatarPos, int act){
+		this.state = state;
+		this.list = list;
+		this.lastAvatarPos = lastAvatarPos;
+		
+		if (act >= 0) this.list.add(act);
 	}
+}
