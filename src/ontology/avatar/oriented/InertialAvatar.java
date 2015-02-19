92d4500664123a7b89973b0e17f68114f350b279
torybash <torybash@gmail.com>
2015-02-19 18:01:09 +0100

    Git fuckup fix 2

--- src/ontology/avatar/oriented/InertialAvatar.java
+++ src/ontology/avatar/oriented/InertialAvatar.java
@@ -16,11 +16,11 @@ import java.awt.*;
  * Time: 17:35
  * This is a Java port from Tom Schaul's VGDL - https://github.com/schaul/py-vgdl
  */
-public class OngoingAvatar extends OrientedAvatar
+public class InertialAvatar extends OrientedAvatar
 {
-    public OngoingAvatar(){}
+    public InertialAvatar(){}
 
-    public OngoingAvatar(Vector2d position, Dimension size, SpriteContent cnt)
+    public InertialAvatar(Vector2d position, Dimension size, SpriteContent cnt)
     {
         //Init the sprite
         this.init(position, size);
@@ -45,21 +45,21 @@ public class OngoingAvatar extends OrientedAvatar
      */
     public void update(Game game)
     {
-        lastMovementType = Types.MOVEMENT.MOVE;
-
-        //Get the input from the player.
-        requestPlayerInput(game);
-
-        //Map from the action mask to a Vector2D action.
-        Vector2d action2D = Utils.processMovementActionKeys(game.ki.getMask());
-
-        //Update the orientation for this cycle's movement,
-        // but only if there was a direction indicated.
-        if(action2D != Types.NONE)
-            this._updateOrientation(action2D);
-
-        //Update movement.
-        super.updatePassive();
+//        lastMovementType = Types.MOVEMENT.MOVE;
+//
+//        //Get the input from the player.
+//        requestPlayerInput(game);
+//
+//        //Map from the action mask to a Vector2D action.
+//        Vector2d action2D = Utils.processMovementActionKeys(game.ki.getMask());
+//
+//        //Update the orientation for this cycle's movement,
+//        // but only if there was a direction indicated.
+//        if(action2D != Types.NONE)
+//            this._updateOrientation(action2D);
+//
+//        //Update movement.
+//        super.updatePassive();
     }
 
 
@@ -70,31 +70,31 @@ public class OngoingAvatar extends OrientedAvatar
      */
     public void move(Game game, boolean[] actionMask)
     {
-        lastMovementType = Types.MOVEMENT.MOVE;
-
-        //Map from the action mask to a Vector2D action.
-        Vector2d action2D = Utils.processMovementActionKeys(actionMask);
-
-        //Update the orientation for this cycle's movement,
-        // but only if there was a direction indicated.
-        if(action2D != Types.NONE)
-            this._updateOrientation(action2D);
-
-        //Update movement.
-        super.updatePassive();
+//        lastMovementType = Types.MOVEMENT.MOVE;
+//
+//        //Map from the action mask to a Vector2D action.
+//        Vector2d action2D = Utils.processMovementActionKeys(actionMask);
+//
+//        //Update the orientation for this cycle's movement,
+//        // but only if there was a direction indicated.
+//        if(action2D != Types.NONE)
+//            this._updateOrientation(action2D);
+//
+//        //Update movement.
+//        super.updatePassive();
     }
 
 
     public VGDLSprite copy()
     {
-        OngoingAvatar newSprite = new OngoingAvatar();
+        InertialAvatar newSprite = new InertialAvatar();
         this.copyTo(newSprite);
         return newSprite;
     }
 
     public void copyTo(VGDLSprite target)
     {
-        OngoingAvatar targetSprite = (OngoingAvatar) target;
+        InertialAvatar targetSprite = (InertialAvatar) target;
         super.copyTo(targetSprite);
     }
 }
